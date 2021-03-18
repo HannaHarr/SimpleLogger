650f9bd4bf37b7dd4198b1f53e2034d80cf32248
Hanna.Harr <Hanna.Harr@WSA-043-71>
2021-03-18 16:29:32 +0300

    Add interfaces

---
+++ SimpleLoggerLibrary/IIMessagePublisher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleLoggerLibrary
+{
+    public interface IMessagePublisher
+    {
+        public void Publish(string message);
+    }
+}
